fd0e68ce148f56328ba5e633d8945e70c8ff7bd2
DanilKladka <106967701+DanilKladka@users.noreply.github.com>
2023-12-05 09:28:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,31 +6,35 @@ import time
 
 def create_database(connection):
     try:
+        # Якщо є бд то ми не створюємо 
         cursor = connection.cursor()
         cursor.execute("USE car_depot")
         print("База даних вже існує.")
     except mysql.connector.Error as err:
+        # Якщо бд немає то створюємо її
         if err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE car_depot")
             print("База даних створена.")
-            
+
             cursor.execute("USE car_depot")
 
+            # Створення таблиці details
             cursor.execute("""
                 CREATE TABLE details (
                     ID INT AUTO_INCREMENT PRIMARY KEY,
-                    title VARCHAR(30),
-                    detailType VARCHAR(30),
-                    manufacturer VARCHAR(30),
-                    brand VARCHAR(30),
-                    model VARCHAR(30),
-                    quantity INT,
-                    price DECIMAL(10, 2)
+                    title VARCHAR(30) NOT NULL,
+                    detailType VARCHAR(30) NOT NULL,
+                    manufacturer VARCHAR(30) NOT NULL,
+                    brand VARCHAR(30) NOT NULL,
+                    model VARCHAR(30) NOT NULL,
+                    quantity INT NOT NULL,
+                    price DECIMAL(10, 2) NOT NULL
                 )
             """)
             print("Таблиця details створена.")
 
+            # Додавання початкових значень
             initial_data = [
                 ('Колінвал', 'Двигун', 'Nissan', 'Nissan', 'Juke', 10, 10000),
                 ('Поршень', 'Двигун', 'Toyota', 'Nissan', 'Juke', 18, 5000),
@@ -55,17 +59,20 @@ def create_database(connection):
     connection.commit()
 
 if __name__ == '__main__':
+    # Підключення до серверу MySQL
     connectToDB = mysql.connector.connect(
         host="localhost",
         user="admin",
         password="admin"
     )
 
+    # Створення бази даних, таблиці та додавання початкових значень
     create_database(connectToDB)
 
     # Затримка перед викликом GetCars()
     time.sleep(1)
-    
+
+    # Запуск додатка
     app = QApplication(sys.argv)
     main_window = StartPage(connectToDB)
     main_window.show()
